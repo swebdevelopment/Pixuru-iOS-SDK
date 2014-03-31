8e9bf9039ddbb817150603176d7520c357c04995
Skyler Whittlesey <skylerwhittlesey@yahoo.com>
2014-03-31 17:20:58 -0500

    new library and example projects

---
+++ Examples/ARC/SDKTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDKTestProject
+//
+//  Created by Skyler Whittlesey on 2/24/14.
+//  Copyright (c) 2014 Skyler Whittlesey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)goPixuru:(id)sender;
+
+@end
---
+++ Examples/ARC/SDKTestProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SDKTestProject
+//
+//  Created by Skyler Whittlesey on 2/24/14.
+//  Copyright (c) 2014 Skyler Whittlesey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
